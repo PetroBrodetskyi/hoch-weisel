7acc063b7544ed9e09e9a55c0eca930cdf8721dc
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-06-21 01:28:13 +0200

    navLanguage updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import icons from '../../assets/icons/icons.svg';
@@ -6,7 +6,14 @@ import css from './Navbar.module.css';
 
 function Navbar() {
   const { t, i18n } = useTranslation();
-  
+  const [selectedLanguage, setSelectedLanguage] = useState(i18n.language);
+
+  useEffect(() => {
+    i18n.on('languageChanged', (lng) => {
+      setSelectedLanguage(lng);
+    });
+  }, [i18n]);
+
   const changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
   };
@@ -19,19 +26,28 @@ function Navbar() {
         <li><Link className={css.link} to="/history">{t('history.title')}</Link></li>
       </ul>
       <div className={css.buttons}>
-        <button className={css.btn} onClick={() => changeLanguage('de')}>
+        <button
+          className={`${css.btn} ${selectedLanguage === 'de' ? css.selected : ''}`}
+          onClick={() => changeLanguage('de')}
+        >
           <svg className={css.icon}>
             <use href={`${icons}#flag-germany`} />
           </svg>
           DE
         </button>
-        <button className={css.btn} onClick={() => changeLanguage('ua')}>
+        <button
+          className={`${css.btn} ${selectedLanguage === 'ua' ? css.selected : ''}`}
+          onClick={() => changeLanguage('ua')}
+        >
           <svg className={css.icon}>
             <use href={`${icons}#flag-ukraine`} />
           </svg>
           UA
         </button>
-        <button className={css.btn} onClick={() => changeLanguage('en')}>
+        <button
+          className={`${css.btn} ${selectedLanguage === 'en' ? css.selected : ''}`}
+          onClick={() => changeLanguage('en')}
+        >
           <svg className={css.icon}>
             <use href={`${icons}#flag-kingdom`} />
           </svg>
