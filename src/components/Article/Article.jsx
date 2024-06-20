80caf230310b42c6fcb521879e455b0c35eec383
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-06-21 01:45:54 +0200

    Home fixed

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import hochweiselLogo from '../../assets/images/hochweisel.png';
+
 import css from './Article.module.css';
 
 const Article = ({ articleKey }) => {
@@ -8,11 +8,10 @@ const Article = ({ articleKey }) => {
 
   return (
     <article className={css.article}>
-      <div>
-      <h2>{t(`${articleKey}.title`)}</h2>
+      <div className={css.container}>
+        <h2>{t(`${articleKey}.title`)}</h2>
         <p>{t(`${articleKey}.content`)}</p>
       </div>
-      <img src={hochweiselLogo} alt={t(`${articleKey}.title`)} className={css.image} />
     </article>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import Article from '../../components/Article/Article';
+import hochweiselLogo from '../../assets/images/hochweisel.png';
 import css from '../Home/Home.module.css';
 
-const Home = () => {
+const Home = ({articleKey}) => {
   const { t } = useTranslation();
 
   return (
     <div className={css.home}>
-      <p>{t('home.description')}</p>
-      <Article articleKey="home.article1" />
+      <div className={css.container}>
+        <p>{t('home.description')}</p>
+        <Article articleKey="home.article1" />
+      </div>
+      <img src={hochweiselLogo} alt={t(`${articleKey}.title`)} className={css.image} />
     </div>
   );
 };
