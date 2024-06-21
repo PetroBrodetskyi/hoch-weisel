fcd285cfa53501bce2af59b92b8ed6e7343980b4
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-06-21 21:19:21 +0200

    icons fixed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hoch-weisel',
-})
+  base: '/hoch-weisel/',
+  optimizeDeps: {
+    include: ['react-i18next']
+  },
+  build: {
+    assetsInlineLimit: 0,
+  }
+});
