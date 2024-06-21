0040e14b5f1a483ff8df0f72ca09a85222b2f7d8
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-06-21 08:57:25 +0200

    Article updated

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-
 import css from './Article.module.css';
 
 const Article = ({ articleKey }) => {
   const { t } = useTranslation();
+  const content = t(`${articleKey}.content`).split('\n\n').map((paragraph, index) => (
+    <p key={index}>{paragraph}</p>
+  ));
 
   return (
     <article className={css.article}>
       <div className={css.container}>
         <h2>{t(`${articleKey}.title`)}</h2>
-        <p>{t(`${articleKey}.content`)}</p>
+        {content}
       </div>
     </article>
   );
