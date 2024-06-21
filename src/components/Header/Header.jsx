7df296e4b97972a47d8b6fc72d16e5d8530f21cd
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-06-21 11:00:59 +0200

    Navbar fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import css from './Header.module.css';
 import hochweiselLogo from '../../assets/images/hochweisel.png';
 import Navbar from '../Navbar/Navbar';
@@ -7,8 +8,12 @@ const Header = () => {
     return (
         <header className={css.headerContainer}>
             <div className={css.header}>
-                <img className={css.logo} src={hochweiselLogo} alt="Hoch-Weisel" />
-                <h1>Hoch-Weisel</h1>
+                <div>
+                    <Link to="/" className={css.logoContainer}>
+                        <img className={css.logo} src={hochweiselLogo} alt="Hoch-Weisel" />
+                        <h1 className={css.link}>Hoch-Weisel</h1>
+                    </Link>
+                </div>
             </div>
             <Navbar />
         </header>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,61 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import icons from '../../assets/icons/icons.svg';
 import css from './Navbar.module.css';
+import iconsUrl from '../../assets/icons/icons.svg';
 
-function Navbar() {
-  const { t, i18n } = useTranslation();
-  const [selectedLanguage, setSelectedLanguage] = useState(i18n.language);
+const Navbar = () => {
+    const { t, i18n } = useTranslation();
+    const [selectedLanguage, setSelectedLanguage] = useState(i18n.language);
 
-  useEffect(() => {
-    i18n.on('languageChanged', (lng) => {
-      setSelectedLanguage(lng);
-    });
-  }, [i18n]);
+    useEffect(() => {
+        const handleLanguageChanged = (lng) => {
+            setSelectedLanguage(lng);
+        };
 
-  const changeLanguage = (lng) => {
-    i18n.changeLanguage(lng);
-  };
+        i18n.on('languageChanged', handleLanguageChanged);
 
-  return (
-    <nav className={css.navLanguage}>
-      <ul className={css.links}>
-        <li><Link className={css.link} to="/">{t('home.title')}</Link></li>
-        <li><Link className={css.link} to="/events">{t('events.title')}</Link></li>
-        <li><Link className={css.link} to="/history">{t('history.title')}</Link></li>
-      </ul>
-      <div className={css.buttons}>
-        <button
-          className={`${css.btn} ${selectedLanguage === 'de' ? css.selected : ''}`}
-          onClick={() => changeLanguage('de')}
-        >
-          <svg className={css.icon}>
-            <use href={`${icons}#flag-germany`} />
-          </svg>
-          DE
-        </button>
-        <button
-          className={`${css.btn} ${selectedLanguage === 'ua' ? css.selected : ''}`}
-          onClick={() => changeLanguage('ua')}
-        >
-          <svg className={css.icon}>
-            <use href={`${icons}#flag-ukraine`} />
-          </svg>
-          UA
-        </button>
-        <button
-          className={`${css.btn} ${selectedLanguage === 'en' ? css.selected : ''}`}
-          onClick={() => changeLanguage('en')}
-        >
-          <svg className={css.icon}>
-            <use href={`${icons}#flag-kingdom`} />
-          </svg>
-          EN
-        </button>
-      </div>
-    </nav>
-  );
-}
+        return () => {
+            i18n.off('languageChanged', handleLanguageChanged);
+        };
+    }, [i18n]);
+
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+    };
+
+    const languages = [
+        { code: 'de', label: 'DE', flag: 'flag-germany' },
+        { code: 'ua', label: 'UA', flag: 'flag-ukraine' },
+        { code: 'en', label: 'EN', flag: 'flag-kingdom' }
+    ];
+
+    return (
+        <nav className={css.navLanguage}>
+            <ul className={css.links}>
+                <li><Link className={css.link} to="/">{t('home.title')}</Link></li>
+                <li><Link className={css.link} to="/events">{t('events.title')}</Link></li>
+                <li><Link className={css.link} to="/history">{t('history.title')}</Link></li>
+            </ul>
+            <div className={css.buttons}>
+                {languages.map(({ code, label, flag }) => (
+                    <label key={code} className={`${css.btn} ${selectedLanguage === code ? css.selected : ''}`}>
+                        <input
+                            type="radio"
+                            name="language"
+                            value={code}
+                            checked={selectedLanguage === code}
+                            onChange={() => changeLanguage(code)}
+                            className={css.radio}
+                        />
+                        <svg className={css.icon}>
+                            <use href={`${iconsUrl}#${flag}`} />
+                        </svg>
+                        {label}
+                    </label>
+                ))}
+            </div>
+        </nav>
+    );
+};
 
 export default Navbar;
